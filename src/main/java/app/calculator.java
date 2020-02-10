416ebd407731bbbba62f40720a9f3c96f597c77c
Ramil Huseynov <raminhuseyli@gmail.com>
2020-02-10 19:38:15 +0400

    calculator

---
+++ src/main/java/app/calculator.java
@@ -0,0 +1,18 @@
+package app;
+
+import java.util.Scanner;
+
+public class calculator {
+
+        public static void main(String[] args) {
+
+            System.out.println("Enter the two numbers");
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            System.out.println("Sum of the a and b = " + Integer.sum(a,b));
+
+
+
+        }
+    }
